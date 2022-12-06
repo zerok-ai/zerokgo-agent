14377337b0abad7ae6e656eeac607cd6e55e714c
rajeevzerok <113543563+rajeevzerok@users.noreply.github.com>
2022-12-06 15:45:19 +0530

    First

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zerok-ai/zerokgo-agent
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("You are free to start trying.")
+	TryItOut()
+}
+
+func TryItOut() {
+	fmt.Println("Be Patient, I'm trying hard.......!!!!!!!!")
+}
