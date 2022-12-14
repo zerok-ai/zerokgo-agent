69cf07239f70c5de3506d4a5898d03a1fae53c03
rajeevzerok <113543563+rajeevzerok@users.noreply.github.com>
2022-12-14 10:15:17 +0530

    First

--- main.go
+++ main.go
@@ -1,16 +1,68 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"strings"
 )
 
-func main() {
-	f, _ := os.Create("out.txt")
-	defer f.Close()
-	f.WriteString("You are free to start trying.")
-	TryItOut(f)
+func fix(dir string) {
+	fset := token.NewFileSet()
+	pkgs, err := parser.ParseDir(fset, dir, nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, pkg := range pkgs {
+		for fileName, file := range pkg.Files {
+			if strings.Contains(fileName, "panic.go") {
+				fmt.Printf("working on file %v\n", fileName)
+				ast.Inspect(file, func(n ast.Node) bool {
+					fn, ok := n.(*ast.FuncDecl)
+					if ok {
+						// current node is a function!
+						if fn.Name.Name == "gopanic" {
+							fmt.Printf("Func name is %v.\n", fn.Name.Name)
+							stmt := []ast.Stmt{
+								&ast.ExprStmt{
+									X: &ast.CallExpr{
+										Fun: &ast.Ident{
+											Name: "print",
+										},
+										Lparen: 32,
+										Args: []ast.Expr{
+											&ast.BasicLit{
+												Kind:  token.STRING,
+												Value: "e",
+											},
+										},
+										Ellipsis: 0,
+									},
+								},
+							}
+							fn.Body.List = append(stmt, fn.Body.List...)
+						}
+					}
+					return true
+				})
+
+				buf := new(bytes.Buffer)
+				err := format.Node(buf, fset, file)
+				if err != nil {
+					fmt.Printf("error: %v\n", err)
+				} else if fileName[len(fileName)-8:] != "_test.go" {
+					ioutil.WriteFile(fileName, buf.Bytes(), 0664)
+				}
+			}
+		}
+	}
 }
 
-func TryItOut(f *os.File) {
-	f.WriteString("Be Patient, I'm trying hard.......!!!!!!!!")
+func main() {
+	fix("/usr/local/Cellar/go/1.19.2/libexec/src/runtime")
 }
